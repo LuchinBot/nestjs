c159f0f95c692a589e86bd0555f4ca4c71bf0826
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-17 11:47:45 -0500

    Modified context

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Demo API')
+    .setDescription('The demo API description')
     .setVersion('1.0')
     .addTag('categories')
     .build();
