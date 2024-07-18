3b5528b983f1a1087176e6e0e7f7812e1796524e
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-18 10:23:22 -0500

    Config Module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,10 +3,10 @@ import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { Category } from './entities/category.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
-  imports: [TypeOrmModule.forFeature([Category])]
+  exports: [TypeOrmModule]
 })
 export class CategoriesModule {}
