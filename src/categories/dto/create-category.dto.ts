a1a4614ed295331a98f9b1e82674a1dd1d7e6908
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-18 10:24:24 -0500

    Add validators

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCategoryDto {}
+import { IsString } from 'class-validator';
+export class CreateCategoryDto {
+  @IsString()
+  readonly title: string;
+  @IsString()
+  readonly slug: string;
+  @IsString()
+  readonly description: string;
+  @IsString()
+  readonly image: string;
+}
