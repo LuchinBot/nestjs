06a39444dd060ad09e9fff8e1705cbadb42a8a8f
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-18 10:22:49 -0500

    Config for POST and GET

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -2,28 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { json } from 'stream/consumers';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    // Retornar un array de categorias
-    return ['category1', 'category2'];
+  constructor(
+    @InjectRepository(Category)
+    private categoriesRepository: Repository<Category>
+  ) {}
+  async create(createCategoryDto: CreateCategoryDto) {
+    // Lógica para crear la categoria
+    const category = this.categoriesRepository.create(createCategoryDto);
+    return this.categoriesRepository.save(category);
   }
 
-  findAll() {
-    const categories = ['category1', 'category2'];
-    return categories;
+  findAll(): Promise<Category[]> {
+    // Obtener todos los registros
+    return this.categoriesRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number): Promise<Category> {
+    // Obtener un solo registro
+    return this.categoriesRepository.findOne({
+      where: { id }
+    });
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
     return `This action updates a #${id} category`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} category`;
   }
 }
