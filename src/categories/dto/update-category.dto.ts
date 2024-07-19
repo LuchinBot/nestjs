c4fa1d9512394225951a3a2c5a82192283b8e47c
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-18 21:09:44 -0500

    Add fields

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
+import { IsString } from 'class-validator';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+  @IsString()
+  title: string;
+  @IsString()
+  slug: string;
+  @IsString()
+  description: string;
+  @IsString()
+  image: string;
+  // se agrega la fecha al updated_at timestamp
+  updated_at: Date;
+}
