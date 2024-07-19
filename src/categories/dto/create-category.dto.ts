e644aad3e8d468e92aaab279e31b6262c424fa4a
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-18 21:27:13 -0500

    No idea xd

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,13 +1,11 @@
 import { IsString } from 'class-validator';
 export class CreateCategoryDto {
   @IsString()
-  readonly title: string;
+  title: string;
   @IsString()
-  readonly slug: string;
+  slug: string;
   @IsString()
-  readonly description: string;
+  description: string;
   @IsString()
-  readonly image: string;
-  // Crear el created_at
-  readonly created_at: Date;
+  image: string;
 }
